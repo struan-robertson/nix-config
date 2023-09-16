3443d89d973b575e94dea54861687b704ef28a5a
Struan Robertson <contact@struanrobertson.co.uk>
2023-09-16 15:15:37 +0100

    fix waybar

--- home-manager/Desktop/waybar/waybar.nix
+++ home-manager/Desktop/waybar/waybar.nix
@@ -1,25 +1,39 @@
+{ pkgs, ... }:
 {
   programs.waybar = {
+  
     enable = true;
+
+#   package = pkgs.waybar.overrideAttrs (oa: {
+#     mesonFlags = (oa.mesonFlags or []) ++ [ "-Dexperimental=true" ];
+#     patches = (oa.patches or []) ++ [
+#       (pkgs.fetchpatch {
+#         name = "fix waybar hyprctl";
+#         url = "https://aur.archlinux.org/cgit/aur.git/plain/hyprctl.patch?h=waybar-hyprland-git";       
+#         sha256 = "sha256-pY3+9Dhi61Jo2cPnBdmn3NUTSA8bAbtgsk2ooj4y7aQ=";  
+#       })
+#     ];
+#   });
 
     settings = {
       mainBar = {
         layer = "top";
         position = "top";
       
-        modules-left = [ "wlr/workspaces" "hyprland/window" ];
+        modules-left = [ "hyprland/workspaces" "hyprland/window" ];
         modules-center = [ "clock" ];
         modules-right = [ "tray" "idle_inhibitor" "network" "cpu" "memory" "disk" "battery" ]; 
 
-        "wlr/workspaces" = {
-          format = "{icon}";
-          format-icons = {
-            "1" = "1 <span font='Font Awesome 6 Free 11'></span>";
-            "2" = "2 <span font='Font Awesome 6 Free 11'></span>";
-            "3" = "3 <span font='Font Awesome 6 Free 11'></span>";
-          };
+        "hyprland/workspaces" = {
+          format = "{name} {icon}";
           on-click = "activate";
           sort-by-number = true;
+          format-icons = {
+            "1" = "<span font='Font Awesome 6 Free'></span>";
+            "2" = "<span font='Font Awesome 6 Free'></span>";
+            "3" = "<span font='Font Awesome 6 Free'></span>";
+            "default" = "";
+          };
         };
 
         "hyprland/window" = {
@@ -30,27 +44,27 @@
         clock = {
           timezone = "Europe/London";
           format = "{:%a %d %b - %H:%M}";
-          tooltip-format = "<big>{:%B %Y}</big>\n<tt><big>{calendar}</big></tt>";
+          tooltip = false;
         };
 
         cpu = {
-          format = "<span font='Font Awesome 6 Free 11'>{icon}</span> {usage}%";
+          format = "<span font='Font Awesome 6 Free'>{icon}</span> {usage}%";
           format-icons = [ "" ];
           escape = true;
           on-click = "wezterm htop";
         };
 
         memory = {
-          format = "<span font='Font Awesome 6 Free 11'>{icon}</span> {}%";
+          format = "<span font='Font Awesome 6 Free'>{icon}</span> {}%";
           format-icons = [ "" ];
           on-click = "wezterm htop";
         };
 
         battery = {
-          format = "<span font='Font Awesome 6 Free 11'>{icon}</span> {capacity}%";
+          format = "<span font='Font Awesome 6 Free'>{icon}</span> {capacity}%";
           format-icons = [ "" "" "" "" "" ];
-          format-charging = "<span font='Font Awesome 6 Free 11'></span> <span font='Font Awesome 6 Free 11'>{icon}</span> {capacity}%";
-          format-full = "<span font='Font Awesome 6 Free 11'></span> <span font='Font Awesome 6 Free 11'>{icon}</span>";
+          format-charging = "<span font='Font Awesome 6 Free'></span> <span font='Font Awesome 6 Free 11'>{icon}</span> {capacity}%";
+          format-full = "<span font='Font Awesome 6 Free'></span> <span font='Font Awesome 6 Free 11'>{icon}</span>";
           interval = 30;
           states = {
             warning = 25;
@@ -63,16 +77,16 @@
           format = "{icon}";
           format-alt = "{ipaddr}/{cidr} {icon}";
           format-alt-click = "click-right";
-          format-wifi = "<span font='Font Awesome 6 Free 11'></span> {essid} ({signalStrength}%)";
+          format-wifi = "<span font='Font Awesome 6 Free'></span> {essid} ({signalStrength}%)";
           format-ethernet = " {ifname}";
-          format-disconnected = "<span font='Font Awesome 6 Free 11'>⚠</span> Disconnected";
+          format-disconnected = "<span font='Font Awesome 6 Free'>⚠</span> Disconnected";
           on-click = "wezterm nmtui";
           tooltip = false;
         };
 
         disk = {
           interval = 30;
-          format = "<span font='Font Awesome 6 Free 11'></span> {percentage_used}%";
+          format = "<span font='Font Awesome 6 Free'></span> {percentage_used}%";
           path = "/";
           on-click = "duc gui /home/struan";
         };
@@ -80,8 +94,8 @@
         idle_inhibitor = {
           format = "{icon}";
           format-icons = {
-            activated = "<span font='Font Awesome 6 Free 11'></span>";
-            deactivated = "<span font='Font Awesome 6 Free 11'></span>";
+            activated = "<span font='Font Awesome 6 Free'></span>";
+            deactivated = "<span font='Font Awesome 6 Free'></span>";
           };
           tooltip = false;
         };
