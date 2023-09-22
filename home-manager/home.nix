12ffcbbcb1f2e00f4a41d166f852a5fba973b058
Struan Robertson <contact@struanrobertson.co.uk>
2023-09-22 18:11:22 +0100

    Add tofi launcher

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -52,7 +52,25 @@
     pavucontrol
     gnumake
     cmake
+    (aspellWithDicts (dicts: with dicts; [ en en-computers en-science ]))
+    tofi
   ];
+
+  xdg.configFile."tofi/config".text = ''
+    width = 200%
+    height = 200%
+    border-width = 0
+    outline-width = 0
+    padding-left = 97%
+    padding-top = 75%
+    result-spacing = 25
+    num-results = 9
+    font = /home/struan/Sync/bin/InterNerdFont-Regular.otf
+    background-color = #000A
+    text-color = #ECEFF4
+    selection-color = #5E81AC
+    prompt-text ="󰑮 "
+  '';
 
   home.pointerCursor = {
     gtk.enable = true;
