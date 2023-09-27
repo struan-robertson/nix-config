2ae38cdd04627faa35552edac6b2aaa2eda8d355
Struan Robertson <contact@struanrobertson.co.uk>
2023-09-27 14:06:08 +0100

    Update Zotero

--- Overlays/home-manager/zotero.nix
+++ Overlays/home-manager/zotero.nix
@@ -1,11 +1,11 @@
 final: prev: {
   zotero = prev.zotero.overrideAttrs (oldAttrs: rec {
-    version = "7.0.0-beta.40%2B24ae34104";
+    version = "7.0.0-beta.42%2B2db19ad4c";
     src = prev.fetchurl {
       url =
         "https://download.zotero.org/client/beta/${version}/Zotero-${version}_linux-x86_64.tar.bz2";
       sha256 =
-        "e2bfcd1bab434f8b4d83269e86bb8aa797ee59bfd04d8b07874a1c68465f2c6f";
+        "a977871f8aeec006be75348e03729208765214e419ab39558e722c751994ecc5";
     };
 
     postPatch = "";
