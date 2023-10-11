6d024750aaf4664ce0d5a325ee70e7093b69440b
Struan Robertson <contact@struanrobertson.co.uk>
2023-10-11 17:50:59 +0100

    Seems like I have to track hardware-configuration.nix annoyingly

---
+++ nixos/hardware-configuration.nix
@@ -0,0 +1,70 @@
+# Use this to contain hardware specific settings, such as uuid which should not be shared between machines
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "vmd" "nvme" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ "dm-snapshot" ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+  boot.initrd.luks.devices.root = {
+  	device = "/dev/disk/by-uuid/dd05da1e-5bc3-4199-a057-edca42596d5f";
+	preLVM = true;
+	allowDiscards = true;  
+  };
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/a0ef6f45-c391-4ee1-a93c-794da96c49ba";
+      fsType = "btrfs";
+      options = [ "subvol=root" "compress=zstd" "noatime" ];
+    };
+
+  fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/a0ef6f45-c391-4ee1-a93c-794da96c49ba";
+      fsType = "btrfs";
+      options = [ "subvol=home" "compress=zstd" "noatime" ];
+    };
+
+  fileSystems."/nix" =
+    { device = "/dev/disk/by-uuid/a0ef6f45-c391-4ee1-a93c-794da96c49ba";
+      fsType = "btrfs";
+      options = [ "subvol=nix" "compress=zstd" "noatime" ];
+    };
+
+  fileSystems."/persist" =
+    { device = "/dev/disk/by-uuid/a0ef6f45-c391-4ee1-a93c-794da96c49ba";
+      fsType = "btrfs";
+      options = [ "subvol=persist" "compress=zstd" "noatime" ];
+    };
+
+  fileSystems."/var/log" =
+    { device = "/dev/disk/by-uuid/a0ef6f45-c391-4ee1-a93c-794da96c49ba";
+      fsType = "btrfs";
+      options = [ "subvol=log" "compress=zstd" "noatime" ];
+      neededForBoot = true;
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/A522-980F";
+      fsType = "vfat";
+    };
+
+  swapDevices =
+    [ { device = "/dev/disk/by-uuid/5591b6af-5b0a-4ba1-8051-5661dd5dda01"; }
+    ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+}
