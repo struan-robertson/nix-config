5ca7f4ead46a1c18110b3842f2cd9288c09439a5
Struan Robertson <contact@struanrobertson.co.uk>
2023-10-11 21:06:14 +0100

    Add tlp power management

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -103,6 +103,13 @@
   # TODO Remove for desktop profile
   services.tlp = {
     enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+    };
   };
 
   programs.fish.enable = true;
