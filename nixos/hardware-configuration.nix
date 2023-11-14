105ec8c54134df28c7ada3b73d6a419d4439aba7
Struan Robertson <contact@struanrobertson.co.uk>
2023-11-14 11:23:39 +0000

    Add hard drives

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -4,41 +4,51 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules =
+    [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ "amdgpu" ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/0d756c0c-94cc-4d43-a0d1-32a3923e40e1";
-      fsType = "btrfs";
-      options = [ "subvol=root" "compress=zstd" "noatime" ];
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/0d756c0c-94cc-4d43-a0d1-32a3923e40e1";
+    fsType = "btrfs";
+    options = [ "subvol=root" "compress=zstd" "noatime" ];
+  };
 
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/0d756c0c-94cc-4d43-a0d1-32a3923e40e1";
-      fsType = "btrfs";
-      options = [ "subvol=home" "compress=zstd" "noatime" ];
-    };
+  fileSystems."/home" = {
+    device = "/dev/disk/by-uuid/0d756c0c-94cc-4d43-a0d1-32a3923e40e1";
+    fsType = "btrfs";
+    options = [ "subvol=home" "compress=zstd" "noatime" ];
+  };
 
-  fileSystems."/nix" =
-    { device = "/dev/disk/by-uuid/0d756c0c-94cc-4d43-a0d1-32a3923e40e1";
-      fsType = "btrfs";
-      options = [ "subvol=nix" "compress=zstd" "noatime" ];
-    };
+  fileSystems."/nix" = {
+    device = "/dev/disk/by-uuid/0d756c0c-94cc-4d43-a0d1-32a3923e40e1";
+    fsType = "btrfs";
+    options = [ "subvol=nix" "compress=zstd" "noatime" ];
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/42DA-51B3";
-      fsType = "vfat";
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/42DA-51B3";
+    fsType = "vfat";
+  };
+
+  fileSystems."/home/struan/Vault" = {
+    device = "/dev/disk/by-uuid/49d4d1c8-6ffe-4bc8-9946-a0284d3d692c";
+    fsType = "btrfs";
+    options = [ "compress=zstd" "noatime" ];
+  };
+
+  fileSystems."/home/struan/Extra" = {
+    device = "/dev/disk/by-uuid/33fd3e32-1610-43d0-930d-4f90ed928c69";
+    fsType = "btrfs";
+    options = [ "compress=zstd" "noatime" ];
+  };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/dfea6aec-8e96-46d0-b122-04505a45d2bd"; }
-    ];
+    [{ device = "/dev/disk/by-uuid/dfea6aec-8e96-46d0-b122-04505a45d2bd"; }];
 
   # Enable BTRFS autoscrub
   services.btrfs.autoScrub.enable = true;
@@ -52,5 +62,6 @@
   # networking.interfaces.wlp6s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
