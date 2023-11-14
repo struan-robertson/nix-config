ac2565ec34a3dc73602a9f02fd5288351dd6ea35
Struan Robertson <contact@struanrobertson.co.uk>
2023-11-14 19:29:24 +0000

    Eww bar

--- home-manager/Desktop/eww/eww.nix
+++ home-manager/Desktop/eww/eww.nix
@@ -1,8 +1,16 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, pkgs-unstable, ... }:
 
 {
   programs.eww = {
     enable = true;
     configDir = ./config;
+    package = pkgs-unstable.eww-wayland;
   };
+
+  home.packages = with pkgs; [
+    jq
+    socat
+    alsa-utils
+    iw
+  ];
 }
