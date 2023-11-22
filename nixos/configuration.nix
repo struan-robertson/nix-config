53bf9d854f9618fd11bd2112b48201bedb2cb2e0
Struan Robertson <contact@struanrobertson.co.uk>
2023-11-22 12:10:55 +0000

    Add udev rule for android devices

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -109,6 +109,11 @@
 
   services.tailscale.enable = true;
 
+  # Dont need root to access android devices
+  services.udev.extraRules = ''
+    SUBSYSTEM=="usb", ATTR{idVendor}=="18d1", MODE="0666", GROUP="plugdev"
+  '';
+
   # Hyprland
   programs.hyprland.enable = true;
   xdg.portal = {
