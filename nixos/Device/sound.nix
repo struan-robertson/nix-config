4963742f84f2bf2cb27a778c0b6a5b8bd4008980
Struan Robertson <contact@struanrobertson.co.uk>
2024-02-12 19:09:05 +0000

    Stop sound from cutting out when pausing video

--- nixos/Device/sound.nix
+++ nixos/Device/sound.nix
@@ -8,4 +8,43 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
+
+  environment.etc."wireplumber/main.lua.d/51-disable-suspension.lua".text = ''
+    table.insert (alsa_monitor.rules, {
+      matches = {
+        {
+          -- Matches all sources.
+          { "node.name", "matches", "alsa_input.*" },
+        },
+        {
+          -- Matches all sinks.
+          { "node.name", "matches", "alsa_output.*" },
+        },
+      },
+      apply_properties = {
+        ["session.suspend-timeout-seconds"] = 0,  -- 0 disables suspend
+      },
+    })
+  '';
+
+  environment.etc."wireplumber/bluetooth.lua.d/51-disable-suspension.lua".text = ''
+    -- Note: bluez_monitor, not alsa_monitor
+    table.insert (bluez_monitor.rules, {
+      matches = {
+        {
+          -- Matches all sources.
+          -- Note: bluez_input, not alsa_input
+          { "node.name", "matches", "bluez_input.*" },
+        },
+        {
+          -- Matches all sinks.
+          -- Note: bluez_output, not alsa_output
+          { "node.name", "matches", "bluez_output.*" },
+        },
+      },
+      apply_properties = {
+        ["session.suspend-timeout-seconds"] = 0,  -- 0 disables suspend
+      },
+    })
+  '';
 }
