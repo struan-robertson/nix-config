969224d026601b1863bbcfc0e9f91fe8533f2812
Struan Robertson <contact@struanrobertson.co.uk>
2024-03-17 22:11:02 +0000

    Add calibre ports

--- nixos/Device/networking.nix
+++ nixos/Device/networking.nix
@@ -4,6 +4,12 @@
   networking.hostName = "nix";
   networking.networkmanager.enable = true;
 
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 9090 ];
+    allowedUDPPorts = [ 9090 ];
+  };
+
   services.mullvad-vpn = {
     enable = true;
     package = pkgs.mullvad-vpn;
